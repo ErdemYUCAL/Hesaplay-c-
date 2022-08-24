2c9982affa891f6f34ccd1f96cf5ce198c6b81c1
ErdemYUCAL <111757631+ErdemYUCAL@users.noreply.github.com>
2022-08-24 09:17:57 +0300

    Add files via upload

---
+++ hesaplayıcı.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+int main() {
+	printf("selamun aleykum");  int x,y;
+	printf("\nlutfen birinci sayiyi giriniz");  scanf("%d", &x);
+	printf("\nlutfen ikinci sayiyi giriniz");  scanf("%d", &y);
+	printf("\ntoplami:%d",x+y);  printf("\nfarki:%d",x-y);
+	printf("\ncarpimi:%d",x*y);  printf("\nbolumu:%d",x/y);
+	getch();  return 0;}
+	//iþlem girmeye gerek yok hepsi gösteriliyo//
